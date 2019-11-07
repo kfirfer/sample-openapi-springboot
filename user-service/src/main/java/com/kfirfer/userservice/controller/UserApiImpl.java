6cb6440ac26e7d5cb6e01457ed2e5e1f1a3dab76
Kfir Fersht <kfirfer@gmail.com>
2019-11-07 04:56:05 -0100

    example implementation

--- user-service/src/main/java/com/kfirfer/userservice/controller/UserApiImpl.java
+++ user-service/src/main/java/com/kfirfer/userservice/controller/UserApiImpl.java
@@ -1,9 +1,18 @@
 package com.kfirfer.userservice.controller;
 
 import com.kfirfer.userservice.api.UserApi;
+import com.kfirfer.userservice.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiImpl implements UserApi {
 
+    @Override
+    public ResponseEntity<User> getUserByName(String username) {
+        User user = new User();
+        user.setUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
