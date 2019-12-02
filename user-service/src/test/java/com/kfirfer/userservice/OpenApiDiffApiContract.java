38ddb40282dafba80d3c8b1d8c1a21edf44b933b
Kfir <kfirfer@gmail.com>
2019-12-02 18:21:02 -0100

    Bump springdoc openapi version & Refactoring tests

--- user-service/src/test/java/com/kfirfer/userservice/OpenApiDiffApiContract.java
+++ user-service/src/test/java/com/kfirfer/userservice/OpenApiDiffApiContract.java
@@ -4,23 +4,46 @@ package com.kfirfer.userservice;
 import com.qdesrame.openapi.diff.OpenApiCompare;
 import com.qdesrame.openapi.diff.model.ChangedOpenApi;
 import com.qdesrame.openapi.diff.output.MarkdownRender;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MainApp.class)
 public class OpenApiDiffApiContract {
 
     private Path resourceDirectory = Paths.get("src", "test", "resources");
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void apiContractTest() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andDo((result) -> {
+                    String swaggerJsonString = result.getResponse().getContentAsString();
+                    FileUtils.writeStringToFile(new File(resourceDirectory + "/swagger/documentation/openapi_from_code.json"), swaggerJsonString, Charset.defaultCharset());
+                });
+
+
         ChangedOpenApi diff = OpenApiCompare.fromLocations(resourceDirectory + "/swagger/documentation/openapi.yml", resourceDirectory + "/swagger/documentation/openapi_from_code.json");
         renderMarkDown(diff);
         Assert.assertEquals(0, diff.getChangedOperations().size());
