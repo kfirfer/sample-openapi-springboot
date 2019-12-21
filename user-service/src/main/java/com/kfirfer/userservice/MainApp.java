80ce9064cc0571519c5416557b8a4fbe62c63035
Kfir <kfirfer@gmail.com>
2019-12-21 22:07:36 +0200

    No need for ComponentScan

--- user-service/src/main/java/com/kfirfer/userservice/MainApp.java
+++ user-service/src/main/java/com/kfirfer/userservice/MainApp.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kfirfer.userservice"})
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
